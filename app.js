06575267f0293a150c8e63071d77df18a7450f88
Viswanatha Reddy <injaviswanathreddy@gmail.com>
2023-01-04 22:22:19 -0900

    variables

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hey hero... welcome to ts");
+function add(n1, n2, showResult, phraseString) {
+    if (showResult) {
+        console.log(phraseString + (n1 + n2));
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 10;
+var printResult = true;
+var phraseString = "Result is :";
+var result = add(number1, number2, printResult, phraseString);
+console.log(result);
